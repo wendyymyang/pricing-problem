2aed8074b0ac8415222e96c90c526a26beb7bd28
Wendy Yang <wyangym@gmail.com>
2016-12-07 20:44:41 -0500

    Using process.argv this time to pass arguments to the priceCalculator function via the command line

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 function priceCalculator(basePrice, numberPeopleOnJob, material) {
+
     var flatMarkupRate = 0.05;
     var peopleMarkupRate = 0.012;
 
@@ -14,3 +15,13 @@ function priceCalculator(basePrice, numberPeopleOnJob, material) {
         return basePricePlusFlatMarkup * (1 + (peopleMarkupRate * numberPeopleOnJob));
     }
 }
+
+var basePrice;
+var numberPeopleOnJob;
+var material;
+
+basePrice = process.argv[2];
+numberPeopleOnJob = process.argv[3];
+material = process.argv[4];
+
+console.log(priceCalculator(basePrice, numberPeopleOnJob, material));
