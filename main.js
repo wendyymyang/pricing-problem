fe9af415426c78d89fbac472947659410d379f7f
Wendy Yang <wyangym@gmail.com>
2016-12-08 13:32:14 -0500

    Added validation in case the user enters in less than five arguments. Gave a variable to the lower-cased material

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 function priceCalculator(basePrice, numberPeopleOnJob, material) {
-
     var flatMarkupRate = 0.05;
     var peopleMarkupRate = 0.012;
 
     var basePricePlusFlatMarkup = basePrice * (1 + flatMarkupRate);
 
-    if (material.toLowerCase() == 'drugs') {
+    var materialInLowerCase = material.toLowerCase();
+
+    if (materialInLowerCase == 'drugs') {
         return basePricePlusFlatMarkup * (1 + (peopleMarkupRate * numberPeopleOnJob) + 0.075);
-    } else if (material.toLowerCase() == 'food') {
+    } else if (materialInLowerCase == 'food') {
         return basePricePlusFlatMarkup * (1 + (peopleMarkupRate * numberPeopleOnJob) + 0.13);
-    } else if (material.toLowerCase() == 'electronics') {
+    } else if (materialInLowerCase == 'electronics') {
         return basePricePlusFlatMarkup * (1 + (peopleMarkupRate * numberPeopleOnJob) + 0.02);
     } else {
         return basePricePlusFlatMarkup * (1 + (peopleMarkupRate * numberPeopleOnJob));
@@ -20,13 +21,19 @@ var basePrice = process.argv[2];
 var numberPeopleOnJob = process.argv[3];
 var material = process.argv[4];
 
-if (isNaN(basePrice)) {
-    console.log('Please enter a numeric value for the base price.')
-} else if (isNaN(numberPeopleOnJob) || Number.isInteger(numberPeopleOnJob) == false) {
-    console.log('Please enter an integer for the number of people on the job.') 
+if (process.argv.length < 5) {
+    console.log('Please enter the base price, the number of people on the job, and the material used.');
 } else {
-    var totalPrice = priceCalculator(basePrice, numberPeopleOnJob, material).toFixed(2);
-    console.log('Total Price: $' + totalPrice);
+    if (isNaN(basePrice)) {
+        console.log('Please enter a numeric value for the base price.');
+    } else if (numberPeopleOnJob % 1 != 0) {
+        console.log('Please enter an integer for the number of people on the job.');
+    } else {
+        var totalCost = priceCalculator(basePrice, numberPeopleOnJob, material).toFixed(2);
+        console.log('Total Cost: $' + totalCost);
+    }
 }
+
+
 
 
