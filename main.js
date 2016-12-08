db23792f03a877bbf344cb109227fd2624743b1a
Wendy Yang <wyangym@gmail.com>
2016-12-07 21:44:16 -0500

    Using prompts in the command line to input the base price, number of people on the job, and the material used

--- main.js
+++ main.js
@@ -16,12 +16,48 @@ function priceCalculator(basePrice, numberPeopleOnJob, material) {
     }
 }
 
-var basePrice;
-var numberPeopleOnJob;
-var material;
+// var basePrice = process.argv[2];
+// var numberPeopleOnJob = process.argv[3];
+// var material = process.argv[4];
 
-basePrice = process.argv[2];
-numberPeopleOnJob = process.argv[3];
-material = process.argv[4];
+// var totalPrice = priceCalculator(basePrice, numberPeopleOnJob, material).toFixed(2);
 
-console.log(priceCalculator(basePrice, numberPeopleOnJob, material));
+// console.log('Total Price: $' + totalPrice);
+
+var prompt = require('prompt');
+
+var calculatorInputs = [
+    {
+        name: 'basePrice',
+        validator: /[0-9]$/,
+        warning: 'Base price can only include numbers and the dollar sign.'
+    },
+    {
+        name: 'numberPeopleOnJob',
+        validator: /[0-9]/,
+        warning: 'The number of people on the job can only include numbers.'
+    },
+    {
+        name: 'material',
+        validator: /[a-z]/,
+        warning: 'Materials can only be entered in lower case letters.'
+    }
+];
+
+prompt.start();
+
+prompt.get(calculatorInputs, function(err, result) {
+
+    if (err) {
+        return onErr(err);
+    }
+
+    var totalPrice = priceCalculator(result.basePrice, result.numberPeopleOnJob, result.material).toFixed(2);
+    console.log('Total Price: $' + totalPrice);
+});
+
+function onErr(err) {
+    console.log(err);
+    return 1;
+}
+
