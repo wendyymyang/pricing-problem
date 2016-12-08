dcd2442ea18061394f49a0d79d05d493087f90d8
Wendy Yang <wyangym@gmail.com>
2016-12-08 11:35:49 -0500

    Decided on using process.argv since it provides for a shorter function. Also included validations for numbers/integers for the base price and number of people on the job

--- main.js
+++ main.js
@@ -16,48 +16,17 @@ function priceCalculator(basePrice, numberPeopleOnJob, material) {
     }
 }
 
-// var basePrice = process.argv[2];
-// var numberPeopleOnJob = process.argv[3];
-// var material = process.argv[4];
+var basePrice = process.argv[2];
+var numberPeopleOnJob = process.argv[3];
+var material = process.argv[4];
 
-// var totalPrice = priceCalculator(basePrice, numberPeopleOnJob, material).toFixed(2);
-
-// console.log('Total Price: $' + totalPrice);
-
-var prompt = require('prompt');
-
-var calculatorInputs = [
-    {
-        name: 'basePrice',
-        validator: /[0-9]$/,
-        warning: 'Base price can only include numbers and the dollar sign.'
-    },
-    {
-        name: 'numberPeopleOnJob',
-        validator: /[0-9]/,
-        warning: 'The number of people on the job can only include numbers.'
-    },
-    {
-        name: 'material',
-        validator: /[a-zA-Z]/,
-        warning: 'Materials can only be entered in lower case letters.'
-    }
-];
-
-prompt.start();
-
-prompt.get(calculatorInputs, function(err, result) {
-
-    if (err) {
-        return onErr(err);
-    }
-
-    var totalPrice = priceCalculator(result.basePrice, result.numberPeopleOnJob, result.material).toFixed(2);
+if (isNaN(basePrice)) {
+    console.log('Please enter a numeric value for the base price.')
+} else if (isNaN(numberPeopleOnJob) || Number.isInteger(numberPeopleOnJob) == false) {
+    console.log('Please enter an integer for the number of people on the job.') 
+} else {
+    var totalPrice = priceCalculator(basePrice, numberPeopleOnJob, material).toFixed(2);
     console.log('Total Price: $' + totalPrice);
-});
-
-function onErr(err) {
-    console.log(err);
-    return 1;
 }
+
 
